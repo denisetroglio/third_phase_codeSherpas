0840a201b03293fc4bedaa47b8ad8a023ee02f68
Denise Troglio <overbeck.denise@gmail.com>
2022-05-10 12:27:53 +0200

    add codesherpas game

--- src/main.js
+++ src/main.js
@@ -2,4 +2,38 @@
 const express = require("express");
 const cors = require("cors");
 
-console.log("Hi everyone! I´m Denise, Jr. Developer looking for new challenges! =)");
+/*console.log(
+  "Hi everyone! I´m Denise, Jr. Developer looking for mi first opportunity =)"
+);*/
+ 
+var count = 0;
+
+function countFunction() {
+  console.log(count);
+  count = count + 1;
+  if (count == 0) {
+    console.log("The gun is chargin...");
+  }
+  if (count == 1) {
+    console.log("Are you ready to loose?");
+  }
+  if (count == 2) {
+    console.log("You should run...");
+  }
+  if (count == 3) {
+    console.log("Are you sure that will stay here? I can be dangerous...");
+  }
+  if (count == 4) {
+    console.log("Shooting!!!!");
+  }
+
+  if (count >= 5) {
+    count = 0;
+  }
+}
+
+setInterval(function () {
+  countFunction();
+}, 1000);
+
+module.exports = {countFunction, count};
--- unit_test/main.test.js
+++ unit_test/main.test.js
@@ -1,1 +1,5 @@
 const main = require("../src/main.js");
+
+test("checkinf if the count are increasing 1 by 1", () => {
+  expect(main.count).toBe(1);
+});
